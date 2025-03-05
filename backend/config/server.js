f5d810d458333fd2f624df6c928ce3da93cc906b
Jose Alvarez Dominguez <josealvarezdominguez00@gmail.com>
2025-03-05 17:45:00 -0300

    movimiento de rutas

--- backend/config/server.js
+++ backend/config/server.js
@@ -1,3 +1,5 @@
+const path = require("path");
+
 module.exports = ({ env }) => ({
   host: env("HOST", "0.0.0.0"),
   port: env.int("PORT", 1337),
@@ -8,6 +10,9 @@ module.exports = ({ env }) => ({
     auth: {
       secret: env("ADMIN_JWT_SECRET"), // Asegúrate de establecer esta variable de entorno
     },
+
+    path: "/admin",
+    build: path.join(__dirname, "build"), // Ruta a la carpeta build en la raíz del proyecto
   },
   webhooks: {
     populateRelations: env.bool("WEBHOOKS_POPULATE_RELATIONS", false),
