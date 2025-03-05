31056a8eaece07cd06f22cdb7cb93f6e5d4ed34a
Jose Alvarez Dominguez <josealvarezdominguez00@gmail.com>
2025-03-05 17:50:04 -0300

    config cors

--- backend/config/middlewares.js
+++ backend/config/middlewares.js
@@ -1,12 +1,18 @@
 module.exports = [
-  'strapi::logger',
-  'strapi::errors',
-  'strapi::security',
-  'strapi::cors',
-  'strapi::poweredBy',
-  'strapi::query',
-  'strapi::body',
-  'strapi::session',
-  'strapi::favicon',
-  'strapi::public',
+  "strapi::logger",
+  "strapi::errors",
+  "strapi::security",
+  {
+    name: "strapi::cors",
+    config: {
+      enabled: true,
+      origin: ["https://uhmo-shop.netlify.app"],
+    },
+  },
+  "strapi::poweredBy",
+  "strapi::query",
+  "strapi::body",
+  "strapi::session",
+  "strapi::favicon",
+  "strapi::public",
 ];
